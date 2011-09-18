7949cd6a4fc62a1d429178a70618d843dd73a560
Juhan.Kundla <juhan.kundla@skype.net>
2011-09-18 17:38:32 +0300

    ClassLoader is now object

--- core/src/main/php/tapeet/ClassLoader.php
+++ core/src/main/php/tapeet/ClassLoader.php
@@ -6,25 +6,58 @@ class ClassLoader {
 
 
 	const CLASS_NAME_PATTERN = '/^[\\a-zA-Z0-9_]+$/';
+	private static $INSTANCE;
+
+
+	private $listeners;
+
+
+	function __construct() {
+		$this->listeners = array();
+	}
+
+
+	function addListener(ClassLoaderListener $listener) {
+		$this->listeners[] = $listener;
+	}
+
+
+	static function get() {
+		return self::$INSTANCE;
+	}
 
 
 	static function init() {
-		spl_autoload_register(array(__CLASS__, 'load'), true);
+		if (self::$INSTANCE !== null) {
+			return;
+		}
+		self::$INSTANCE = new ClassLoader();
+		spl_autoload_register(array(self::$INSTANCE, 'load'), true);
 	}
 
 
-	static function load($type) {
-		if (! preg_match(self::CLASS_NAME_PATTERN, $type)) {
+	function load($className) {
+		if (! preg_match(self::CLASS_NAME_PATTERN, $className)) {
 			return false;
 		}
 
-		$file = str_replace('\\', DIRECTORY_SEPARATOR, $type) . '.php';
+		$file = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
 		if (stream_resolve_include_path($file)) {
 			include_once $file;
 		}
-		return class_exists($type, false);
+
+		if (! class_exists($className, false)) {
+			return false;
+		}
+
+		foreach ($this->listeners as $listener) {
+			$listener->onLoad($className);
+		}
+
+		return true;
 	}
 
 }
+
 
 ClassLoader::init();
