ca7c8d11dd925c4a47c40c16ec20578b06f7d063
Juhan.Kundla <juhan.kundla@skype.net>
2011-11-11 15:43:12 +0200

    Removed references to obsolete IOCProxy from the FilterChain

--- core/src/main/php/tapeet/FilterChain.php
+++ core/src/main/php/tapeet/FilterChain.php
@@ -2,23 +2,17 @@
 namespace tapeet;
 
 
-use \tapeet\ioc\IOCProxy;
-
-
 class FilterChain {
 
 
-	/** @Service */
 	public $filters;
 
 
 	public function doFilter() {
-		$filterClass = array_shift($this->filters);
-		if ($filterClass != null) {
-			$filter = new IOCProxy($filterClass);
+		$filter = array_shift($this->filters);
+		if ($filter !== null) {
 			$filter->doFilter($this);
 		}
 	}
-
 
 }
