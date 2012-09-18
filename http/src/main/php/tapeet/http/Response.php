1940828de0f444256edf46ab928cf3ae1441bb68
Juhan.Kundla <juhan.kundla@skype.net>
2012-09-18 14:44:19 +0300

    http/Response: more constants for different HTTP statuses

--- http/src/main/php/tapeet/http/Response.php
+++ http/src/main/php/tapeet/http/Response.php
@@ -5,9 +5,13 @@ namespace tapeet\http;
 class Response {
 
 
+	const SC_OK = 200;
+	const SC_CREATED = 201;
+	const SC_ACCEPTED = 202;
 	const SC_BAD_REQUEST = 400;
 	const SC_FORBIDDEN = 403;
 	const SC_NOT_FOUND = 404;
+	const SC_METHOD_NOT_ALLOWED = 405;
 
 
 	function encodeRedirectURL($url) {
