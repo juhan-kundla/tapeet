04dcd0bd52ebd985b3f6d9414bfef36a84bef63a
Juhan.Kundla <juhan.kundla@skype.net>
2012-09-20 23:27:34 +0300

    CORE: shortened Bootstrap

--- core/src/main/php/tapeet/Bootstrap.php
+++ core/src/main/php/tapeet/Bootstrap.php
@@ -1,16 +1,11 @@
 <?php
 namespace tapeet;
 
-
 require_once 'tapeet/ClassLoader.php';
-use \tapeet\annotation\AnnotationProcessor;
-use \tapeet\ioc\ContextUtils;
-
-
-$classLoader = new ClassLoader();
+$classLoader = new \tapeet\ClassLoader();
 spl_autoload_register(array($classLoader, 'load'), true);
 
-$classLoader->addListener(new AnnotationProcessor());
+$classLoader->addListener(new \tapeet\annotation\AnnotationProcessor());
 
-$context = ContextUtils::getContext();
+$context = \tapeet\ioc\ContextUtils::getContext();
 $context->add('_tapeet_core_classLoader', $classLoader);
